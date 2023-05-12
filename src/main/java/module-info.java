45abe053d9dcbf8f93e49831e1b6a7635c5fc69f
korsem <korneliasem@gmail.com>
2023-05-12 17:22:59 +0200

    #1

---
+++ src/main/java/module-info.java
@@ -0,0 +1,17 @@
+module com.example.alarm {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+    requires javafx.swing;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens com.example.alarm to javafx.fxml;
+    exports com.example.alarm;
+}
